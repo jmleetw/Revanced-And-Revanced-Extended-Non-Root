b837c2e9eea8453c9d61a91c02aaf220ce762689
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-03-01 10:55:10 +0700

    fix

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -119,7 +119,7 @@ revanced_dl(){
 	revanced_dl
 	# Patch Lightroom:
 	get_patches_key "lightroom"
- 	url="https://adobe-lightroom-mobile.en.uptodown.com/android/download/1033600808" #Use uptodown because apkmirror always ask pass Cloudflare on this app
+ 	url="https://adobe-lightroom-mobile.en.uptodown.com/android/download/1033600808-x" #Use uptodown because apkmirror always ask pass Cloudflare on this app
 	url="https://dw.uptodown.com/dwn/$(req "$url" - | $pup -p --charset utf-8 'button#detail-download-button attr{data-url}')"
 	req "$url" "lightroom-beta.apk"
 	patch "lightroom-beta" "revanced"
